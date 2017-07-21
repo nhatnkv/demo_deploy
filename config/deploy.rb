62602e31777507c6fee2630b502dd538f3807a50
nhatnkv@gmail.com <nhatnkv@gmail.com>
2017-07-21 07:59:14 +0700

    remove link_file

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :branch, :master
 set :deploy_to, '/var/www/demo_deploy'
 
 set :pty, true
-set :linked_files, %w{config/database.yml config/secrets.yml}
+set :linked_files, %w{config/database.yml}
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 set :deploy_via, :remote_cache
 set :keep_releases, 5
