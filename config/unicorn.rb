8d2ea3901afe3049ee19674b6b8770be7bc8fc4d
nhatnkv@gmail.com <nhatnkv@gmail.com>
2017-07-21 09:46:48 +0700

    add unicorn.rb

---
+++ config/unicorn.rb
@@ -0,0 +1,9 @@
+root = '/var/www/demo_deploy/current'
+working_directory root
+pid "#{root}/tmp/pids/unicorn.pid"
+stderr_path "#{root}/log/unicorn.error.log"
+stdout_path "#{root}/log/unicorn.access.log"
+ 
+listen '/var/sockets/unicorn.demo_deploy.sock'
+worker_processes 2
+timeout 30
