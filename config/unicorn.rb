79b5f56210e3429046ca9ee81be62e7293162598
nhatnkv@gmail.com <nhatnkv@gmail.com>
2017-07-24 11:42:09 +0700

    edit path

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,6 +4,6 @@ pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.error.log"
 stdout_path "#{root}/log/unicorn.access.log"
  
-listen '/var/sockets/unicorn.demo_deploy.sock'
+listen '/var/www/demo_deploy/shared/tmp/sockets/unicorn.demo_deploy.sock'
 worker_processes 2
 timeout 30
